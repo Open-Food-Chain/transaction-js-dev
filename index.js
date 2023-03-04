f3608c86bcc754720a95f48048554221f8427762
diesmaster <chris@vanmaarseveen.net>
2023-03-04 05:38:38 +0000

    first step to moduality

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ const {
 const BigNumber = require('@ethersproject/bignumber');
 const coinSelect = require('coinselect')
 
+//vars
 const sendTo = "RMNSVdQhbSzBVTGt2SVFtBg7sTbB8mXYwN"
 const changeAddress = "RMNSVdQhbSzBVTGt2SVFtBg7sTbB8mXYwN"
 const wif = "UvjpBLS27ZhBdCyw2hQNrTksQkLWCEvybf4CiqyC6vJNM3cb6Qio"
-const IADDRESS_VERSION = 102
 
+const IADDRESS_VERSION = 102
 const utxo_url = config.get('explorer.utxo_url') 
 const send_url = config.get('explorer.send_url')
 const name_network = config.get('networks.name')
@@ -48,94 +49,108 @@ const generateOutputScript = (destHash, destVersion, isCC) => {
   }
 };
 
-var utxos
+const fixElements = (utxo) => {
+	utxo['value'] = utxo['satoshis']
+}
 
-axios.get(utxo_url)
-  .then(res => { 
-	var  tx = "test"
-	let inputValueSats = 0
-    	const inputValues = []
-    	let isCC = false;
-    	let isChangeCC = false;
 
-	const txb = new TransactionBuilder(networks[name_network]);
-	utxos = res.data
+//add keys, target, and amount as vars
+const maketx = () => {
+
+
+	var utxos
+
+	axios.get(utxo_url)
+  	  .then(res => { 
+		var  tx = "test"
+		let inputValueSats = 0
+    		const inputValues = []
+    		let isCC = false;
+    		let isChangeCC = false;
+		const network = networks[name_network]
 	
-	let targets = [
-      		{
-        	address: sendTo,
-        	value: utxos[0]['satoshis']-1 ,
-     		},
-    	];
+		const txb = new TransactionBuilder(network);
+		utxos = res.data
+	
+		let targets = [
+      			{
+        		address: sendTo,
+        		value: utxos[0]['satoshis']-1 ,
+     			},
+    		];
 
-	utxos = [utxos[2]]
-	utxos[0]['value'] = utxos[0]['satoshis']
+	
+		utxos.forEach(fixElements)
 
-	let {inputs, outputs} = coinSelect(utxos, targets, 0);
+		let {inputs, outputs} = coinSelect(utxos, targets, 0);
 
-	if (!outputs) {
-      		throw new Error(
-        		'Insufficient funds. Failed to calculate acceptable transaction amount with fee of ' +
-          		"0.1" +
-          		'.',
+		if (!outputs) {
+      			throw new Error(
+        			'Insufficient funds. Failed to calculate acceptable transaction amount with fee of ' +
+          			"0.1" +
+          			'.',
+      				);
+    		}
+
+    		txb.setVersion(4)
+    		txb.setExpiryHeight(
+      		Number(
+			"605000"
+      			),
+    		);
+
+	    	txb.setVersionGroupId(0x892f2085);
+		for (const input of inputs) {
+      			const {txid, vout, scriptPubKey, value} = input;
+			console.log(input)
+      			const inputValueBigNum = value
+
+      			inputValues.push(inputValueBigNum)
+      			inputValueSats = inputValueSats + inputValueBigNum
+
+      			txb.addInput(
+        			txid,
+        			vout,
+        			Transaction.DEFAULT_SEQUENCE,
+        			Buffer.from(scriptPubKey, 'hex'),
       			);
-    	}
+    		}
+		const valueSats = inputValueSats - 1
+		const addr = address.fromBase58Check(sendTo);
+    		const selfAddr = address.fromBase58Check(sendTo);
 
-    	txb.setVersion(4)
-    	txb.setExpiryHeight(
-      	Number(
-	"605000"
-      		),
-    	);
-    	txb.setVersionGroupId(0x892f2085);
-	for (const input of inputs) {
-      		const {txid, vout, scriptPubKey, value} = input;
-		console.log(input)
-      		const inputValueBigNum = value
+		let actualFeeSats = inputValueSats - valueSats 
 
-      		inputValues.push(inputValueBigNum)
-      		inputValueSats = inputValueSats + inputValueBigNum
+    		const outputScript = generateOutputScript(addr.hash, addr.version, isCC)
 
-      		txb.addInput(
-        		txid,
-        		vout,
-        		Transaction.DEFAULT_SEQUENCE,
-        		Buffer.from(scriptPubKey, 'hex'),
-      		);
-    	}
-	const valueSats = inputValueSats - 1
-	const addr = address.fromBase58Check(sendTo);
-    	const selfAddr = address.fromBase58Check(sendTo);
+		txb.addOutput(outputScript, valueSats);
 
-	let actualFeeSats = inputValueSats - valueSats 
+		let keyPair = ECPair.fromWIF(wif, network)
 
-    	const outputScript = generateOutputScript(addr.hash, addr.version, isCC)
+		for (let i = 0; i < txb.inputs.length; i++) {
+      			txb.sign(
+        			i,
+        			keyPair,
+        			null,
+        			Transaction.SIGHASH_ALL,
+        			inputValues[i],
+      			);
+    		}
+		var tx = txb.build();
+	
+		tx = tx.toHex()
 
-	txb.addOutput(outputScript, valueSats);
+		tx = { 'rawtx': tx }
 
-	let keyPair = ECPair.fromWIF(wif, network)
+		axios.post(send_url, tx)
+          	  .then( res => { console.log("txid ", res.data) })
+        	  .catch(err => { console.log('Error: ', err.message) });  
+       	})
+  	.catch(err => {
+    	console.log('Error: ', err.message);
+  	});
 
-	for (let i = 0; i < txb.inputs.length; i++) {
-      		txb.sign(
-        		i,
-        		keyPair,
-        		null,
-        		Transaction.SIGHASH_ALL,
-        		inputValues[i],
-      		);
-    	}
-	var tx = txb.build();
-	console.log(tx)
-	tx = tx.toHex()
+}
 
-	tx = { 'rawtx': tx }
-
-	axios.post(send_url, tx)
-          .then( res => { console.log("txid ", res.data) })
-          .catch(err => { console.log('Error: ', err.message) });  
-       })
-  .catch(err => {
-    console.log('Error: ', err.message);
-  });
-
+maketx()
 
