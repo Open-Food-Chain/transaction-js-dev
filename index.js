9dba3ac3d672f6ccf32179075e72107cc91f40ac
diesmaster <chris@vanmaarseveen.net>
2023-10-16 07:31:47 +0000

    JC-2956

--- index.js
+++ index.js
@@ -27,45 +27,66 @@ const test_batch = {
         "created_at": "2023-09-25T08:21:45.070925Z",
         "percentage": null
     }
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
 
-const wallet = {
-  WALLET_DELIVERY_DATE: "DELIVERY_DATE",
-  WALLET_JULIAN_START: "JULIAN_START",
-  WALLET_JULIAN_STOP: "JULIAN_STOP",
-  WALLET_BB_DATE: "BB_DATE",
-  WALLET_PROD_DATE: "PROD_DATE",
-  WALLET_ORIGIN_COUNTRY: "ORIGIN_COUNTRY",
-  WALLET_TIN: "TIN",
-  WALLET_PON: "PON",
-  WALLET_PRODUCTID: "PRODUCTID",
-  WALLET_MASS_BALANCE: "MASS_BALANCE"
-};
+function generateRandomDate() {
+  const start = new Date(2000, 0, 1);
+  const end = new Date();
+  const randomDate = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  const year = randomDate.getFullYear();
+  const month = String(randomDate.getMonth() + 1).padStart(2, '0');
+  const day = String(randomDate.getDate()).padStart(2, '0');
+  
+  return `${year}-${month}-${day}`;
+}
 
-const test_bnfp = "187556"
+function createRandomJSON() {
+  return {
+    "id": generateRandomString(36),
+    "anfp": String(Math.floor(Math.random() * 100000000)),
+    "dfp": generateRandomString(16),
+    "bnfp": String(Math.floor(Math.random() * 1000000)),
+    "pds": generateRandomDate(),
+    "pde": generateRandomDate(),
+    "jds": String(Math.floor(Math.random() * 10)),
+    "jde": String(Math.floor(Math.random() * 10)),
+    "bbd": generateRandomDate(),
+    "pc": generateRandomString(2),
+    "pl": generateRandomString(8),
+    "rmn": String(Math.floor(Math.random() * 10000000000)),
+    "pon": String(Math.floor(Math.random() * 1000000)),
+    "pop": String(Math.floor(Math.random() * 1000)),
+    "mass": String(Math.random().toFixed(2)),
+    "raw_json": Buffer.from(JSON.stringify({ randomKey: generateRandomString(5) })).toString('base64'),
+    "integrity_details": Math.random() > 0.5 ? null : generateRandomString(10),
+    "created_at": new Date().toISOString(),
+    "percentage": Math.random() > 0.5 ? null : String(Math.floor(Math.random() * 100))
+  };
+}
 
-res = bitGoUTXO.ECPair.fromWIF("UvjpBLS27ZhBdCyw2hQNrTksQkLWCEvybf4CiqyC6vJNM3cb6Qio", bitGoUTXO.networks[name_network])
-addy = res.getAddress()
+async function sample_batch( wif ){
+  //const wif = "UvjpBLS27ZhBdCyw2hQNrTksQkLWCEvybf4CiqyC6vJNM3cb6Qio"
 
-console.log(addy)
+  const res = bitGoUTXO.ECPair.fromWIF(wif, bitGoUTXO.networks[name_network])
+  
+  const test_batch = createRandomJSON() 
 
-ec_pairs = get_all_ecpairs( test_batch, res )
+  console.log(`test batch: ${JSON.stringify(test_batch)}`)
 
-baseAddy = "RMNSVdQhbSzBVTGt2SVFtBg7sTbB8mXYwN"
-baseWIF = "UvjpBLS27ZhBdCyw2hQNrTksQkLWCEvybf4CiqyC6vJNM3cb6Qio";
+  const ec_pairs = get_all_ecpairs( test_batch, res )
 
-test_string = "Description here";
+  const tx1 = await send_batch_transactions( ec_pairs, test_batch, res ) 
+  console.log(`batchtx: ${JSON.stringify(tx1)}`)
 
-console.log(ec_pairs);
+  return tx1
+}
 
-
-/*( async () => { 
-  const tx1 = await fund_offline_wallets( ec_pairs, baseAddy, baseWIF ) 
-  console.log(`fund: ${JSON.stringify(tx1)}`)
-})();
-*/
-
-( async () => { 
-  const tx2 = await send_batch_transactions( ec_pairs, test_batch, res )
-  console.log(`batch: ${JSON.stringify(tx2)}`)
-})();
-
+const wif = "UvjpBLS27ZhBdCyw2hQNrTksQkLWCEvybf4CiqyC6vJNM3cb6Qio"
+const ret = sample_batch( wif )
